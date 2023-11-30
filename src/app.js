923482d2859592ae24e06aa2074202e6eb5dd234
k-0107 <k.tkm0107@gmail.com>
2023-11-30 15:53:37 +0900

    learn-reactとの連携

--- src/app.js
+++ src/app.js
@@ -4,20 +4,25 @@ const app = express();
 const mongoose = require("mongoose");
 const Photo = require("./models/photos");
 
-mongoose
-  .connect("mongodb://localhost:27017/photo")
-  .then(() => {
-    console.log("MongoDBコネクションOK!");
-  })
-  .catch((err) => {
-    console.log("MongoDBコネクションエラー!");
-    console.log(err);
-  });
+// mongoose
+//   .connect("mongodb://localhost:27017/photo")
+//   .then(() => {
+//     console.log("MongoDBコネクションOK!");
+//   })
+//   .catch((err) => {
+//     console.log("MongoDBコネクションエラー!");
+//     console.log(err);
+//   });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
+console.log(path.join(__dirname, "../../learn-react/dist"));
+
+app.use(
+  "/photo",
+  express.static(path.join(__dirname, "../../learn-react/dist"))
+);
 
 app.listen(8080, (req, res) => {
   console.log("ポート8080で待機中...");
