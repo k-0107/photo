faa4a30ac5a458b38e5bac974d700e6ae6d2145e
k-0107 <k.tkm0107@gmail.com>
2023-12-15 16:26:00 +0900

    __dirname変更

--- src/app.mjs
+++ src/app.mjs
@@ -1,24 +1,14 @@
 import path from "path";
 import express from "express";
+const __dirname = path.resolve();
 const app = express();
-const router = express.Router();
-
-// Hello Worldを返却するAPI
-router.get("/", function (req, res, next) {
-  res.json({ message: "Hello World" });
-});
-
-module.exports = router;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-console.log(path.join(__dirname, "../../learn-react/dist"));
+console.log(__dirname);
 
-app.use(
-  "/photo",
-  express.static(path.join(__dirname, "../../learn-react/dist"))
-);
+app.use("/photo", express.static(path.join(__dirname, "learn-react/dist")));
 
 app.listen(8080, (req, res) => {
   console.log("ポート8080で待機中...");
